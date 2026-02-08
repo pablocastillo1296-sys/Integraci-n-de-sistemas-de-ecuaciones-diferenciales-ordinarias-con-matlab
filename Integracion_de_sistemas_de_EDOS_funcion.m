cd071e53adb9cc39c100e91907064e57f9450658
pablocastillo1296-sys <pablo.castillo1296@alumnos.udg.mx>
2026-02-08 15:30:12 -0600

    Update Integracion_de_sistemas_de_EDOS_funcion.m

--- Integracion_de_sistemas_de_EDOS_funcion.m
+++ Integracion_de_sistemas_de_EDOS_funcion.m
@@ -1,12 +1,12 @@
 function dx = Integracion_de_sistemas_de_EDOS_funcion(~,x)
-Ip  = 0.0079;
-Mc  = 0.7031;
-lp  = 0.3302;
-Mp  = 0.23;
-Fc  = 0;
-Beq = 4.3;
+Ip  = 0.2;
+Mc  = 0.2;
+lp  = 0.3;
+Mp  = 0.1;
+Fc  = 2;
+Beq = 0.25;
 g   = 9.81;
-Bp  = 0.0024;
+Bp  = 0.2;
 
 xc     = x(1);
 xcdot  = x(2);
@@ -18,7 +18,7 @@ D = (Mc+Mp)*Ip + Mc*Mp*lp^2 + Mp^2*lp^2*sin(alpha)^2;
 xdd = ( (Ip + Mp*lp^2)*Fc ...
       + Mp^2*lp^2*g*cos(alpha)*sin(alpha) ...
       - (Ip + Mp*lp^2)*Beq*xcdot ...
-      - (Ip*Mp*lp - Mp^2*lp^3)*adot^2*sin(alpha) ...
+      - (Ip*Mp*lp + Mp^2*lp^3)*adot^2*sin(alpha) ...
       - Mp*lp*Bp*adot*cos(alpha) ) / D;
 
 add = ( (Mc + Mp)*Mp*g*lp*sin(alpha) ...
@@ -32,5 +32,6 @@ dx(1) = xcdot;
 dx(2) = xdd;
 dx(3) = adot;
 dx(4) = add;
+
 
 end
